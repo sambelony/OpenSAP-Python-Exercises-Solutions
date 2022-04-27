c4aaead1f0abf343d039811a317fadbe019c4efa
Sergei <60666684+sambelony@users.noreply.github.com>
2022-04-27 19:30:02 +0200

    Bonus task added

--- week2exercises.py
+++ week2exercises.py
@@ -75,3 +75,19 @@ for plan in range(len(planned_sales)):
         prod_quant = abs(planned_sales[plan] - stock_level)
         stock_level = 0
     print("Production quantity month", plan + 1, "-", prod_quant)
+
+
+# BONUS
+# Write a Python program that prints the numbers from 1 to 100.
+# If the number is dividable by 3 print Fizz, if the number is dividable by 5 print Buzz instead of the number.
+# If the number is dividable by 3 and 5 print FizzBuzz.
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)

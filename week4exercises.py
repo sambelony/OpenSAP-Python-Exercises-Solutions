5cabe0c28c368fd8bc11010dd37cebfa5c63ae6e
Sergei <60666684+sambelony@users.noreply.github.com>
2022-05-04 17:08:30 +0200

    Exercises (w/o week assessment)

--- week4exercises.py
+++ week4exercises.py
@@ -42,3 +42,52 @@ with open("invoice_data.txt", "r") as file:
     list_tuples = [line.split() for line in file]
 for item in list_tuples:
     print(f"{item[0]:15s}{int(item[1]):3d}{float(item[2]):7.2f}{(float(item[1])*float(item[2])):8.2f}")
+
+
+
+
+
+
+
+# You probably know the game “Rock, Paper, Scissors”. A game for two players.
+# Each player has three options, namely rock, paper, scissors, which are formed by the player’s hand.
+# The rules are quite easy: - rock beats scissors - scissors beats paper - paper beats rock.
+# If both players have chosen the same object, it’s a draw.
+# In the following, we play 100 consecutive games.
+# Each player has to hand in a file consisting of one letter per line.
+# The letters are either “R”, “P” or “S”.
+# Write a Python program that reads two files player1.txt and player2.txt.
+# These files are organized according to the above rules.
+# The program should compare both inputs and calculate how many games have been won by player1, by player2
+# and how many games ended in a draw.
+# The results are written into a file result.txt
+
+player1_wins = 0
+player2_wins = 0
+draws = 0
+with open("player1.txt", "r") as file1:
+    player1_list = [line.strip() for line in file1]
+with open("player2.txt", "r") as file2:
+    player2_list = [line.strip() for line in file2]
+for rounds in range(len(player1_list)):
+    if player1_list[rounds] == player2_list[rounds]:
+        draws += 1
+    elif player1_list[rounds] == "R":
+        if player2_list[rounds] == "S":
+            player1_wins += 1
+        else:
+            player2_wins += 1
+    elif player1_list[rounds] == "P":
+        if player2_list[rounds] == "R":
+            player1_wins += 1
+        else:
+            player2_wins += 1
+    elif player1_list[rounds] == "S":
+        if player2_list[rounds] == "P":
+            player1_wins += 1
+        else:
+            player2_wins += 1
+print(player1_list)
+print(player2_list)
+with open("result.txt", "w") as file3:
+    file3.write(f"Player1 wins: {player1_wins}\nPlayer2 wins: {player2_wins}\nDraws: {draws}\n")
